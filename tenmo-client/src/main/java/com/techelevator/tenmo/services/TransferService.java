4d6bbc0aee5c822cb1ada2c3ff11728890355cd9
Chad <c.swiklinski@gmail.com>
2022-06-15 13:20:32 -0400

    Progress

--- tenmo-client/src/main/java/com/techelevator/tenmo/services/TransferService.java
+++ tenmo-client/src/main/java/com/techelevator/tenmo/services/TransferService.java
@@ -1,4 +1,40 @@
 package com.techelevator.tenmo.services;
 
+import com.techelevator.tenmo.model.Transfer;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.RestClientResponseException;
+import org.springframework.web.client.RestTemplate;
+
 public class TransferService {
+
+    private static final String BASE_URL = "http://localhost:8080/";
+    private RestTemplate restTemplate = new RestTemplate();
+    private String authToken= null;
+
+    public void setAuthToken(String authToken){
+        this.authToken=authToken;
+    }
+
+    private HttpEntity<Transfer> makeTransferEntity(Transfer transfer){
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        httpHeaders.setBearerAuth(authToken);
+        return new HttpEntity<>(transfer, httpHeaders);
+    }
+
+    public boolean create(Transfer transfer){
+        boolean success = false;
+
+        try {
+            restTemplate.put(BASE_URL + "transfer", makeTransferEntity(transfer));
+            success = true;
+        } catch (RestClientResponseException | ResourceAccessException e){
+            System.out.println(e.getMessage());
+        }
+        return success;
+    }
+
 }
